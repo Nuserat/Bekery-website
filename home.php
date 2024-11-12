548988df53cf8de2c7c746115e61f655119517e1
Nushera Tajrin <148541493+Nuserat@users.noreply.github.com>
2024-11-12 11:09:39 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fjalla+One|Libre+Baskerville">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="style.css">
+	<title>Home Page</title>
+</head>
+
+<body style="background-color: #c7d7c7;">
+	<header style="background-color: #c6b5dc">
+		<nav>
+			<div class="navsec"></div>
+			<!--	<img src="Screenshot 2024-10-30 195735.png" alt="" style="width:5%; height: 5%;margin-top: 0%;display: block;margin-left: auto;margin-right: auto;">   -->
+			<ul>
+				<li><a href="#home1">ORDER ONLINE</a></li>
+				<li><a href="#home">GROCERY</a></li>
+				<li><a href="#join">JOIN US</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<div id="home1" class="leftsection">
+			<p class="centered" style="font-family:Sofia;">SWEET BEKARY</p>
+			<p class="lftc">Our biggest launch ever is here:<br> frozen-to-thaw Banana Pudding made <br>with real
+				bananas and whipped<br> to perfection</p>
+			<button onclick="window.location.href='shop.html'" class="button2" style=" top: 50%;">Shop Now</button>
+			<img src="baking-pastry-cta-2up.jpg" alt="Banana Pudding" class="center">
+		</div>
+		<section id="home" class="CollectionTile_cover">
+			<div id="sample" class="sec-content">
+				<h2 style="font-family:Lucida Handwriting; text-align: center;font-size: 200%;">Take A Bite!</h2>
+				<div class="se-section">
+					<img src="brawnie.jpg" alt="Strawberry Brownies">
+					<div class="track-info">
+						<h3>STRAWBERRY BROWNIES</h3>
+						<p>One layers of super-rich Strawberry caBrownieske and silky buttercream means pure, indulgent
+							bliss</p>
+					</div>
+				</div>
+				<div class="se-section">
+					<img src="cake.jpg" alt="Chocolate Cake">
+					<div class="track-info">
+						<h3>MOUTHWATERING CAKES</h3>
+						<p>Three layers of super-rich chocolate cake and silky chocolate buttercream means pure,
+							indulgent bliss</p>
+					</div>
+				</div>
+				<div class="se-section">
+					<img src="pie.jpg" alt="Blueberry Pie">
+					<div class="track-info">
+						<h3>YUMMY PIES</h3>
+						<p>Juicy blueberries top our timeless vanilla bean-infused cheesecake, finished with a graham
+							cracker crust</p>
+					</div>
+				</div>
+				<div class="se-section">
+					<img src="cookie.jpg" alt="Delicious Cookies">
+					<div class="track-info">
+						<h3>DELICIOUS COOKIES</h3>
+						<p>A sumptuous treat with a golden, buttery base that is perfectly balanced</p>
+					</div>
+				</div>
+
+			</div>
+		</section>
+		<div id="join" class="leftsection" style="background-color:#fcc9d0;">
+			<p class="centered2" style="font-family:Sofia; color: rgb(108, 8, 8);">Join Us</p>
+			<p class="lftc2" style=" color: rgb(108, 8, 8);">Our catering, gifting and events team is ready to fill your
+				event or <br>celebration with a custom selection of Magnolia Bakery's best <br> creations. No occasion
+				is too large—or too small!</p>
+			<button onclick="window.location.href=''" class="button" style="   bottom: -125%;left: 65%;">GET
+				STARTED</button>
+			<img src="Screenshot 2024-10-30 225455.png" alt="Join Us" style="width:40%;">
+		</div>
+		<div>
+			<h2
+				style="font-family:Sofia;text-align: center;font-size: 250%;left: 50%;top: 270%;position: absolute;transform: translate(-50%, -50%);">
+				We Deliver Everywhere!</h2>
+			<img src="done.jpg" alt="Banana Pudding" style="width:100%;margin-top: 0%;">
+			<div class="center2">
+				<h4 style="font-family: Lucida Sans;">We make it easy for you to get Magnolia Bakery's best, wherever
+					you are. Order now for nationwide shipping, <br>place an order to pickup at your local shop, or get
+					in touch with our team to arrange custom catering for your next event.</h4>
+				<button onclick="window.location.href=''" class="button" style=" top: 135%;left: 40%;">GET
+					STARTED</button>
+			</div>
+		</div>
+		<footer class="footer_title">
+			<div>
+				<div class="rowsection" style="">
+					<h2>Have a question?<br>We're always here to lend a helping hand. </h2>
+					<p>Consumer Care Team hours are Monday-Friday, 9am - 5pm EST</p>
+				</div>
+				<div class="large" style="">
+					<h2>Contact Info</h2>
+					<p><i class="fa fa-map-marker" style="width:30px"></i> Chicago, US</p>
+					<p><i class="fa fa-phone" style="width:30px"></i> Phone: +00 151515</p>
+					<p><i class="fa fa-envelope" style="width:30px"> </i><a href="mailto:someone@example.com">Email:
+							mail@mail.com</a></p>
+				</div>
+				<div class="padding" style="">
+					<h2>Follow</h2>
+					<a href="https://www.facebook.com/"><i class="fa fa-facebook-official"></i></a>
+					<a href="https://www.instagram.com"><i class="fa fa-instagram"></i></a>
+					<a href="https://www.snapchat.com"><i class="fa fa-snapchat"></i></a>
+					<a href="https://www.flickr.com"><i class="fa fa-flickr"></i></a>
+					<a href="https://www.pinterest.com"> <i class="fa fa-pinterest"></i>
+						<a href="https://www.twitter.com"><i class="fa fa-twitter"></i></a>
+						<a href="https://www.linkedin.com"><i class="fa fa-linkedin"></i></a>
+				</div>
+			</div>
+		</footer>
+	</main>
+</body>
+
+</html>
---
+++ shop.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fjalla+One|Libre+Baskerville">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="style.css">
+	<title>shop Page</title>
+</head>
+
+<body>
+	<header style="background-color: whitesmoke">
+		<nav>
+			<div class="navsec"></div>
+			<!--	<img src="Screenshot 2024-10-30 195735.png" alt="" style="width:5%; height: 5%;margin-top: 0%;display: block;margin-left: auto;margin-right: auto;">   -->
+			<ul>
+				<li><a href="home.html">Home</a></li>
+				<li><a href="#home">GROCERY</a></li>
+				<li><a href="#join">JOIN US</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section class="section2">
+		<div class="about">
+			<h1>Our<span>Store</span></h1>
+		</div>
+		<div class="allCakes">
+			<p id="all">ALL</p>
+			<p id="cakes">CAKES</p>
+			<p id="cupcakes">CUPCAKES</p>
+			<p id="Others">OTHER</p>
+			<p id="doughnuts">DOUGHNUTS</p>
+		</div>
+		<div class="search">
+			<p>
+				<i class="fa fa-search"></i>
+			</p>
+			<input type="text" placeholder="item....">
+		</div>
+		<div class="pictures">
+			<div class="pics1">
+				<div class="imgcart"> <img src="choco.jpg" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Other Item</p>
+					<p>$5</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="Cupcakes-1.jpg" alt="" style="height: 150%; width: 150%;">
+					<p class="movecart">
+						<button onclick="window.location.href='details.html'" class="btn">
+							<img src="cart.png" alt="" style="width: 100%; height: 100%;"></button>
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Cupcake Item</p>
+					<p>$5</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="cake.avif" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Cake Item</p>
+					<p>$5</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="do3.webp" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Doughnut Item</p>
+					<p>$5</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="choco1.jpg" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Other Item</p>
+					<p>$10</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="cupcake6.jpg" alt="" style="width: 100%; height: 100%;">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Cupcake Item</p>
+					<p>$10</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="cake.jpg"">
+					<p class=" movecart">
+					<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Cake Item</p>
+					<p>$10</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="do.jpg" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Doughnut Item</p>
+					<p>$10</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="choco3.jpg" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Other Item</p>
+					<p>$15</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="cupcake3.jpg" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Cupcake Item</p>
+					<p>$15</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="cake2.webp" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Cake Item</p>
+					<p>$15</p>
+				</div>
+			</div>
+
+
+			<div class="pics1">
+				<div class="imgcart"> <img src="do1.jpg" alt="">
+					<p class="movecart">
+						<img src="cart.png" alt="" style="width: 100%; height: 100%;">
+					</p>
+				</div>
+
+				<div class="caption">
+					<p id="word">Doughnut Item</p>
+					<p>$15</p>
+				</div>
+			</div>
+
+
+		</div>
+	</section>
+	<script src="main.js"></script>
+	<footer class="footer_title">
+		<div>
+			<div class="rowsection">
+				<h2>Have a question?<br>We're always here to lend a helping hand. </h2>
+				<p>Consumer Care Team hours are Monday-Friday, 9am - 5pm EST</p>
+			</div>
+			<div class="large">
+				<h2>Contact Info</h2>
+				<p><i class="fa fa-map-marker" style="width:30px"></i> Chicago, US</p>
+				<p><i class="fa fa-phone" style="width:30px"></i> Phone: +00 151515</p>
+				<p><i class="fa fa-envelope" style="width:30px"> </i><a href="mailto:someone@example.com">Email:
+						mail@mail.com</a></p>
+			</div>
+			<div class="padding">
+				<h2>Follow</h2>
+				<a href="https://www.facebook.com/"><i class="fa fa-facebook-official"></i></a>
+				<a href="https://www.instagram.com"><i class="fa fa-instagram"></i></a>
+				<a href="https://www.snapchat.com"><i class="fa fa-snapchat"></i></a>
+				<a href="https://www.flickr.com"><i class="fa fa-flickr"></i></a>
+				<a href="https://www.pinterest.com"> <i class="fa fa-pinterest"></i>
+					<a href="https://www.twitter.com"><i class="fa fa-twitter"></i></a>
+					<a href="https://www.linkedin.com"><i class="fa fa-linkedin"></i></a>
+			</div>
+		</div>
+	</footer>
+	</main>
+</body>
+
+</html>
+
+<!--  https://github.com/sn-tin/cake-shop  -->
